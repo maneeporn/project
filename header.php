85190dae6e699eac4e0932614c4e15343b729281
tummappht <p.ongartitthichai@gmail.com>
2019-04-21 01:06:31 +0700

    add return to top function

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link type="text/css" rel="stylesheet" href="main.css">
     <!-- libraries JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.0.js" integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src="js/return_to_top.js"></script>
@@ -29,4 +29,4 @@
     </div>
     
     <!-- Return to Top -->
-    <a href="javascript:" id="return-to-top"><i class="icon-chevron-up"></i></a>
+    <button id="return_to_top"><i class="fas fa-chevron-up"></i></button>
--- index.php
+++ index.php
@@ -7,9 +7,11 @@
     				$result = mysqli_query($connect,$userquery);
             while ($row = mysqli_fetch_assoc($result))
             			{
-            				echo '<div class="col-6">
-                          <img class="img-fluid" src="image/'.$row['picture1'].'">
-                          <a class="btn btn-outline-dark" href="product.php?id='.$row['product_id'].'" role="button">see more</a>
+                    echo '<div class="col-6">
+                            <div class="product_card">
+                              <img class="img-fluid" src="image/'.$row['picture1'].'">
+                              <a class="btn btn-outline-dark" href="product.php?id='.$row['product_id'].'" role="button">see more</a>
+                            </div>
                           </div>';
             			}
     	?>
